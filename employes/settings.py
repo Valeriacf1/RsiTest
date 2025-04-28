8adff0901b0217d90d5c15890426eca56bd0364f
Valeriacf1 <92828533+Valeriacf1@users.noreply.github.com>
2025-04-28 00:26:40 -0700

    Primer commit - Proyecto base

--- employes/settings.py
+++ employes/settings.py
@@ -100,24 +100,24 @@ WSGI_APPLICATION = 'employes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dbemployees',
+#         'USER': 'postgres',
+#         'PASSWORD': 'lrs1997as!',
+#         'HOST': '172.17.0.2',  # O la dirección IP del contenedor de PostgreSQL
+#         'PORT': '5432',  # El puerto por defecto de PostgreSQL
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbemployees',
-        'USER': 'postgres',
-        'PASSWORD': 'lrs1997as!',
-        'HOST': '172.17.0.2',  # O la dirección IP del contenedor de PostgreSQL
-        'PORT': '5432',  # El puerto por defecto de PostgreSQL
-    }
+  'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
-
-
-#DATABASES = {
-#   'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 
 # Password validation
